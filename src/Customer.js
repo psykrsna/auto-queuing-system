de48db9b3f445c902669eb7e5321b4604ba6ec9c
psykrsna <saivnm5@gmail.com>
2017-12-19 12:31:52 +0530

    Created Customer App
    Deleted unnecessary template files.
    Created UI with basic layout.
    Integrated form with the createRequest API.

---
+++ src/Customer.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Customer extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      customer: ''
+    };
+  }
+
+  handleCustomerChange = (event) => {
+    this.setState({ customer: event.target.value });
+  }
+
+  submitRequest = () => {
+    var comp = this;
+    var apiRoot = localStorage.getItem('apiRoot');
+    var data = {
+        query: "mutation createRequest($input: RequestInput) { \n createRequest(input: $input) \n }",
+        variables: {
+          input: {
+            customer: this.state.customer
+          }
+        }
+    };
+
+    axios({
+      method: 'post',
+      url: apiRoot+'/api',
+      data: data
+    }).then( response => {
+      if(response.data.data.createRequest === true){
+        alert('Request successfully received. A ride will be allotted to you shortly!');
+        comp.setState({
+          customer: ''
+        });
+      }
+    }).catch( error => {
+      alert('There was an error submitting the request');
+      console.log(error);
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="customer-app">
+        <div>
+          <h1>Hire A Ride</h1>
+        </div>
+        <div>  
+          <label> 
+            Customer ID: 
+            <input 
+              type="number" 
+              value={this.state.customer} 
+              onChange={this.handleCustomerChange}
+            />
+          </label>
+        </div>
+        <br/>
+        <div>
+          <button onClick={this.submitRequest} >Ride Now</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Customer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import './app.css';
+import Customer from './Customer';
+import config from './config';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+if(config.DEBUG){
+  localStorage.setItem('apiRoot', 'http://localhost:5000');
+}
+else{
+  localStorage.setItem('apiRoot', 'http://server.herokuapp.com');
+}
+
+class Routes extends React.Component{
+  render() {
+    return (
+      <Router>
+          <Route exact path="/customerapp" component={Customer} />
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
