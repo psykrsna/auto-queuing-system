cb3aa2f9aa4eb19fc692c4f806189fa3945e44fa
psykrsna <saivnm5@gmail.com>
2017-12-19 11:55:30 +0530

    Wrote API to create ride request

--- db/migrations/0001.sql
+++ db/migrations/0001.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `request` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `customerId` int(11) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
   `driverId` int(11) DEFAULT NULL,
-  `createdAt` int(11) DEFAULT NULL,
-  `selectedAt` int(11) DEFAULT NULL,
-  `completedAt` int(11) DEFAULT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `selectedAt` datetime DEFAULT NULL,
+  `completedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- src/app/api.js
+++ src/app/api.js
@@ -2,12 +2,21 @@ var { graphql, buildSchema } = require('graphql');
 var db = require('../db');
 
 var schema = buildSchema(`
+  input RequestInput{
+    customer: Int!
+  }
+
   type Query {
     test: String!
   }
+
+  type Mutation {
+    createRequest(input: RequestInput): Boolean
+}
 `);
 
 var root = {
+
   test: () => {
     let sql = "select 'Hello World!' as test;";
     return db.query(sql).then( result => {
@@ -16,7 +25,19 @@ var root = {
     .catch( error => {
       console.log(error);
     })
+  },
+
+  createRequest: (data, request) => {
+    let customerId = db.escape(data.input.customer);
+    let sql = "INSERT INTO request (customerId) VALUES ("+customerId+");";
+    return db.query(sql).then( result => {
+      return true;
+    })
+    .catch( error => {
+      console.log(error);
+    })
   }
+
 };
 
 module.exports = {
